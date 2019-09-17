50e6cc952391d283dda18c9d36b00f6ffe30c066
Achmad Fachrudin <a2fachrudin@gmail.com>
2019-09-17 23:09:51 +0700

    subscribe navigation event

--- app/src/main/java/com/project/app/BaseApplication.kt
+++ app/src/main/java/com/project/app/BaseApplication.kt
@@ -6,10 +6,14 @@ import androidx.multidex.MultiDex
 import com.ashokvarma.gander.Gander
 import com.ashokvarma.gander.imdb.GanderIMDB
 import com.project.app.module.*
+import com.project.app.navigation.NavigationRoute
 import com.project.data.module.databaseModule
 import com.project.data.module.networkModule
 import com.project.data.module.repositoryModule
 import com.project.data.PrefManager
+import com.project.framework.navigation.NavigationEvent
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -21,6 +25,9 @@ class BaseApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        EventBus.getDefault().register(this)
+
+        /* preference */
         PrefManager.init(this)
 
         /* gander */
@@ -38,5 +45,15 @@ class BaseApplication : Application() {
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(base)
         MultiDex.install(this)
+    }
+
+    @Subscribe
+    fun onEvent(navigationEvent: NavigationEvent) {
+        NavigationRoute.executeEvent(navigationEvent)
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+        EventBus.getDefault().unregister(this)
     }
 }

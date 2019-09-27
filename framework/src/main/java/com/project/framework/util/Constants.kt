03543521083e4e0fdce74a1d1e3f3505058f94f7
Achmad Fachrudin <a2fachrudin@gmail.com>
2019-09-27 09:19:22 +0700

    update request code

--- framework/src/main/java/com/project/framework/util/Constants.kt
+++ framework/src/main/java/com/project/framework/util/Constants.kt
@@ -4,7 +4,8 @@ package com.project.framework.util
  * Created by achmad.fachrudin on 21-Mar-19
  */
 object Constants {
-    const val PERMISSION_REQUEST_CODE = 111
+    const val REQUEST_CODE_PERMISSION = 7011
+    const val REQUEST_CODE_OTHER = 7022
     const val LANGUAGE = "id"
     const val COUNTRY = "ID"
 }

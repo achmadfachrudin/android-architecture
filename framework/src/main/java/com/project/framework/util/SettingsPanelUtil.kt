0484de6a030d1b1aff33ce3da17d0d627a993eb1
Achmad Fachrudin <a2fachrudin@gmail.com>
2019-09-27 09:19:03 +0700

    add settings panel

---
+++ framework/src/main/java/com/project/framework/util/SettingsPanelUtil.kt
@@ -0,0 +1,11 @@
+package com.project.framework.util
+
+import android.content.Intent
+import android.provider.Settings
+
+/**
+ * Created by achmad.fachrudin on 27-Sep-19
+ */
+object SettingsPanel {
+    val intentInternetConnection = Intent(Settings.Panel.ACTION_INTERNET_CONNECTIVITY)
+}
